95566016a3e9e92bf4bf47d8575816bd2c263ca6
sokhyg9016 <sokhyg9016@gmail.com>
2019-08-13 00:37:54 +0900

    초기 커밋.

---
+++ 19_08_10_C_Practice/main.c
@@ -0,0 +1,12 @@
+#pragma warning(disable :4996)
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char str1[14] = "Good Morning";
+	char str2[] = "Good Morning";
+	char str3[10] = "My name is";
+
+	return 0;
+}
